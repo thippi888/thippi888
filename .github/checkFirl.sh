7e307e4b370140908981fe94b5cdd6053f9c88ae
thippi888 <thippi888@gmail.com>
2022-01-31 18:36:30 +0900

    ommit

--- .github/checkFirl.sh
+++ .github/checkFirl.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-echo "Hello"
+
+FILE1="test.txt"
+
+if [ ! -e $FILE ]; then
+curl -o ./te.txt "https://raw.githubusercontent.com/thippi888/aws/main/test.txt"
+else
+curl -o ./te.txt "https://raw.githubusercontent.com/thippi888/aws/main/test.txt"
+fi
