ee7447bb2f349107fb982958e75202b95930e9f2
thippi888 <thippi888@gmail.com>
2022-02-01 14:22:07 +0900

    ff

--- .github/checkFirl.sh
+++ .github/checkFirl.sh
@@ -3,7 +3,7 @@
 FILE1="test.txt"
 
 if [ ! -e $FILE ]; then
-curl -o ./te.txt "https://raw.githubusercontent.com/thippi888/aws/main/test.txt"
+curl -O  "https://raw.githubusercontent.com/thippi888/aws/main/test.txt"
 else
-curl -o ./te.txt "https://raw.githubusercontent.com/thippi888/aws/main/test.txt"
+curl -O  "https://raw.githubusercontent.com/thippi888/aws/main/test.txt"
 fi
